8d4065f59b0f3e809a4eb68684ab713ead8d23a0
AbhijitKumarPandey <156564538+AbhijitKumarPandey@users.noreply.github.com>
2024-03-19 20:59:02 +0530

    Add files via upload

---
+++ Reverse_Char.java
@@ -0,0 +1,10 @@
+
+public class Reverse_Char {
+	public static void main(String[] args) {
+		StringBuilder str = new StringBuilder("Abhijit Pandey");
+		System.out.println(str);
+		str.reverse();
+		System.out.println(str);
+	}
+
+}
