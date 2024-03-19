fcb227cbc5357bedb853e74e76aa6829804c4846
AbhijitKumarPandey <156564538+AbhijitKumarPandey@users.noreply.github.com>
2024-03-19 20:58:42 +0530

    Add files via upload

---
+++ Delete_Char.java
@@ -0,0 +1,9 @@
+
+public class Delete_Char {
+	public static void main(String[] args) {
+		StringBuilder str = new StringBuilder("Abhijit");
+		str.deleteCharAt(3);
+		System.out.println(str);
+	}
+
+}
