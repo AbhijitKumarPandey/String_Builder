954128c9ac686b582a9f8544f9cc3bac45591b6f
AbhijitKumarPandey <156564538+AbhijitKumarPandey@users.noreply.github.com>
2024-03-19 20:57:54 +0530

    Add files via upload

---
+++ Set_CharAt.java
@@ -0,0 +1,11 @@
+
+public class Set_CharAt {
+	public static void main(String[] args) {
+		StringBuilder str = new StringBuilder("Abhijit");
+		System.out.println(str);
+		str.setCharAt(1, 'm');
+		System.out.println(str);
+		
+	}
+
+}
