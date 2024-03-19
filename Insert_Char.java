80b8e4122bb63c646140199db289ceabfc59b47f
AbhijitKumarPandey <156564538+AbhijitKumarPandey@users.noreply.github.com>
2024-03-19 20:58:18 +0530

    Add files via upload

---
+++ Insert_Char.java
@@ -0,0 +1,9 @@
+
+public class Insert_Char {
+	public static void main(String[] args) {
+		StringBuilder str = new StringBuilder("Suruchi");
+		str.insert(0, 'A');
+		System.out.println(str);
+	}
+
+}
